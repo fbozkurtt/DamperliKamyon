af7358154bf1da2911923ceb2802f3084fa6eb61
Furkan <Furkan@FURKANPC>
2020-11-19 02:19:10 +0300

    release 1.1

--- src/damperlikamyon/DamperliKamyon.java
+++ src/damperlikamyon/DamperliKamyon.java
@@ -11,19 +11,19 @@ import java.util.Scanner;
 public class DamperliKamyon {
 
     public static Random rastgele;
+    
     public static int zaman;
     public static int yükleyiciMeşgulSüresi;
     public static int tartıMeşgulSüresi;
+    
     public static ArrayList<Kamyon> kamyonlar;
     public static ArrayList<Kamyon> yükleyiciSırası;
     public static ArrayList<Kamyon> tartımSırası;
     public static GelecektekiOlaylarListesi gelecektekiOlaylarListesi;
+    
     public static int yüklemedekiKamyonSayısı;
     public static int tartımdakiKamyonSayısı;
 
-    /**
-     * @param args the command line arguments
-     */
     public static void başlat() {
         rastgele = new Random();
         zaman = 0;
@@ -33,14 +33,18 @@ public class DamperliKamyon {
         yükleyiciSırası = new ArrayList<>();
         tartımSırası = new ArrayList<>();
         gelecektekiOlaylarListesi = new GelecektekiOlaylarListesi();
+        
         for (int i = 1; i <= 6; i++) {
             kamyonlar.add(new Kamyon("DT" + i));
         }
+        
         yükleyiciSırası.add(kamyonlar.get(3));//DT4
         yükleyiciSırası.add(kamyonlar.get(4));//DT5
         yükleyiciSırası.add(kamyonlar.get(5));//DT6
+        
         yüklemedekiKamyonSayısı = 2;
         tartımdakiKamyonSayısı = 1;
+        
         gelecektekiOlaylarListesi.add(new Olay("Tartim_Bitis", rastgele, kamyonlar.get(0)));//DT1
         gelecektekiOlaylarListesi.add(new Olay("Yukleme_Bitis", rastgele, kamyonlar.get(1)));//DT2
         gelecektekiOlaylarListesi.add(new Olay("Yukleme_Bitis", rastgele, kamyonlar.get(2)));//DT3
@@ -48,30 +52,39 @@ public class DamperliKamyon {
 
     public static void main(String[] args) {
         başlat();
-        int simülasyonSüresi = 0;
+        
+        int simülasyonSüresi;
         Scanner girdi = new Scanner(System.in);
         System.out.print("Simülasyon kaç birim zaman çalışsın?\n>:");
         simülasyonSüresi = girdi.nextInt();
+        
         System.out.println("ZAMAN | KAMYON | YÜKLEYİCİ MEŞGUL S. | TARTI MEŞGUL S. | OLAY");
-        while (zaman <= simülasyonSüresi) {
+        
+        while (true) {
             Olay gerçekleştirilecekOlay = minimumSüreliOlay(gelecektekiOlaylarListesi);
 
             if (zaman + gerçekleştirilecekOlay.süre() >= simülasyonSüresi) {
                 break;
             }
+            
             gelecektekiOlaylarListesi.remove(gerçekleştirilecekOlay);
+            
             if (yüklemedekiKamyonSayısı > 0) {
                 yükleyiciMeşgulSüresi += gerçekleştirilecekOlay.süre() * yüklemedekiKamyonSayısı;
             }
+            
             if (tartımdakiKamyonSayısı > 0) {
                 tartıMeşgulSüresi += gerçekleştirilecekOlay.süre();
             }
+            
             for (int i = 0; i < gelecektekiOlaylarListesi.size(); i++) {
                 gelecektekiOlaylarListesi.get(i).süreyiAzalt(gerçekleştirilecekOlay.süre());
             }
+            
             if ("Tartim_Bitis".equals(gerçekleştirilecekOlay.olayTipi())) {
                 tartımdakiKamyonSayısı = 0;
                 gelecektekiOlaylarListesi.add(new Olay("Yuklemeye_Varis", rastgele, gerçekleştirilecekOlay.kamyon()));
+                
                 if (tartımSırası.size() > 0) {
                     tartımdakiKamyonSayısı = 1;
                     Kamyon tartımSırasıÖndekiKamyon = tartımSırası.get(0);
@@ -79,14 +92,17 @@ public class DamperliKamyon {
                     tartımSırası.remove(tartımSırasıÖndekiKamyon);
                 }
             }
+            
             if ("Yukleme_Bitis".equals(gerçekleştirilecekOlay.olayTipi())) {
                 yüklemedekiKamyonSayısı--;
+                
                 if (tartımdakiKamyonSayısı == 0) {
                     tartımdakiKamyonSayısı = 1;
                     gelecektekiOlaylarListesi.add(new Olay("Tartim_Bitis", rastgele, gerçekleştirilecekOlay.kamyon()));
                 } else {
                     tartımSırası.add(gerçekleştirilecekOlay.kamyon());
                 }
+                
                 if (yüklemedekiKamyonSayısı < 2) {
                     if (yükleyiciSırası.size() > 0) {
                         yüklemedekiKamyonSayısı++;
@@ -96,6 +112,7 @@ public class DamperliKamyon {
                     }
                 }
             }
+            
             if ("Yuklemeye_Varis".equals(gerçekleştirilecekOlay.olayTipi())) {
                 if (yüklemedekiKamyonSayısı < 2) {
                     yüklemedekiKamyonSayısı++;
@@ -104,7 +121,9 @@ public class DamperliKamyon {
                     yükleyiciSırası.add(gerçekleştirilecekOlay.kamyon());
                 }
             }
+            
             zaman += gerçekleştirilecekOlay.süre();
+            
             System.out.println(zaman + "       " + gerçekleştirilecekOlay.kamyon().Name() + "               " + yükleyiciMeşgulSüresi + "                  " + tartıMeşgulSüresi + "         " + gerçekleştirilecekOlay.olayTipi());
         }
         System.out.println("Yükleyicinin meşgul süresi: " + yükleyiciMeşgulSüresi + "\nTartının meşgul süresi " + tartıMeşgulSüresi);
